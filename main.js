8decb5885132c077969ef3a6845f1a3996c04eaa
Amaransh <90545826+Amaransh@users.noreply.github.com>
2021-10-23 15:25:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var image = [
+ "https://scontent.fpat3-2.fna.fbcdn.net/v/t1.6435-9/39177564_1949918018402272_8428532190622515200_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=y4Zike80isEAX9UTmE_&_nc_ht=scontent.fpat3-2.fna&oh=631f08173c48dc4cd320fea77018c2a7&oe=6197AAD6",
+ "https://scontent.fpat3-3.fna.fbcdn.net/v/t1.6435-9/39142995_1949918081735599_1810207863474225152_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=g2y6Jm6qgScAX_rz4ZY&_nc_ht=scontent.fpat3-3.fna&oh=df2bfb65603fe0ae7f886440f32cb067&oe=619794DF",
+ "https://scontent.fpat3-2.fna.fbcdn.net/v/t1.6435-9/39199508_1949918195068921_7313303662602747904_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=F8IlBwrnKCsAX894PYC&_nc_ht=scontent.fpat3-2.fna&oh=6b898a8e71345ca9f5db4e68c3ffbe3e&oe=6197AA8D",
+ "https://scontent.fpat3-3.fna.fbcdn.net/v/t1.6435-9/39211769_1949918338402240_4979812243802685440_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=hYWgiOtdbAMAX8EZiFk&tn=79dSeYE7qMw-zj47&_nc_ht=scontent.fpat3-3.fna&oh=eb5898fe769f32d056121221816b1090&oe=61982837",
+];
+var i = 0;
+function nextslide() {
+    document.getElementById("campus").src=images[i];
+  i++;
+  document.getElementById("campus").src= "https://th.bing.com/th/id/OIP.uud9W0aOWeXAb0KMNgClhwHaHX?w=164&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7"
+}
